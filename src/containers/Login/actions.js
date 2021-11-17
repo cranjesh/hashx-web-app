d2138f42316be8fb3f78b8b423d8b41579f7c10a
cranjesh <63446800+cranjesh@users.noreply.github.com>
2021-11-17 13:10:35 +1100

    Update actions.js

--- src/containers/Login/actions.js
+++ src/containers/Login/actions.js
@@ -1,24 +1,24 @@
 import axios from 'axios'
 import { SETUP_SESSION } from "../../state/actionTypes";
 
-export function setupUserSession(payload) {
-  return { type: SETUP_SESSION, payload };
+export function setupUserSession(sessionData) {
+  return { type: SETUP_SESSION, sessionData };
 }
 
 export function loginSetSession(email, password) {
   return async function (dispatch) {
-      console.log('thunk login', email, password)
-      const loginResp = await axios.post(
-          'https://hashx-login-service.herokuapp.com/loginWithPassword',
-          {
-              Username: password,
-              Email: email,
-              SaltedHash: password,
-              Lat: 1,
-              Long: 1
-          })
-      console.log('thunk loginResp', loginResp)
-      const sessionData = loginResp.data
-      dispatch(setupUserSession(sessionData))
+    console.log('thunk login', email, password)
+    const loginResp = await axios.post(
+      'https://hashx-login-service.herokuapp.com/loginWithPassword',
+      {
+        Username: password,
+        Email: email,
+        SaltedHash: password,
+        Lat: 1,
+        Long: 1
+      })
+    console.log('thunk loginResp', loginResp)
+    const sessionData = loginResp.data
+    dispatch(setupUserSession(sessionData))
   }
- }
+}
