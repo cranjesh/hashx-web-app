7c4c2655ac2d85cef440c9e0685ba9816758bb0c
cranjesh <63446800+cranjesh@users.noreply.github.com>
2021-12-03 20:10:50 +1100

    Update store.js

--- src/state/store.js
+++ src/state/store.js
@@ -1,8 +1,10 @@
 import {createStore, applyMiddleware, compose} from 'redux';
 import appReducer from "./reducer";
 import logger from 'redux-logger'
+import thunk from 'redux-thunk';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(appReducer, composeEnhancers(applyMiddleware(logger)));
+// const store = createStore(appReducer, composeEnhancers(applyMiddleware(logger)));
+const store = createStore(appReducer, composeEnhancers(applyMiddleware(thunk, logger)));
 
 export default store;
